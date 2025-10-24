286f991581687483455239c29127e05ab63d6a42
name400 <132720802+name400@users.noreply.github.com>
2025-10-24 09:05:39 +0900

    Update monster.py

--- monster.py
+++ monster.py
@@ -1,12 +1,13 @@
 import random
 
 class Monster:
-    def __init__(self, name, hp, atk, exp, gold):
+    def __init__(self, name, hp, atk, exp, gold, element="neutral"):
         self.name = name
         self.hp = hp
         self.atk = atk
         self.exp = exp
         self.gold = gold
+        self.element = element
 
     def attack(self):
-        return random.randint(self.atk - 2, self.atk + 2)
+        return random.randint(max(1, self.atk - 2), self.atk + 2)
